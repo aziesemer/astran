d13fb167b1714cf181f7e07794dc1af68c459090
Adriel <adrieljr@gmail.com>
2014-01-04 12:21:55 -0200

    GUI fixes

--- wxSightGL/nbproject/Makefile-variables.mk
+++ wxSightGL/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
 CND_ARTIFACT_NAME_Debug=wxsightgl
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/wxsightgl
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/wxsightgl
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
 CND_PACKAGE_NAME_Debug=wxsightgl.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/wxsightgl.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/wxsightgl.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux-x86
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
 CND_ARTIFACT_NAME_Release=wxsightgl
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/wxsightgl
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/wxsightgl
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
 CND_PACKAGE_NAME_Release=wxsightgl.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/wxsightgl.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/wxsightgl.tar
 #
 # include compiler specific variables
 #
