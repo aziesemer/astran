9c4a75cd6c966e6c97e54cce3bc026a5b6e05c73
Adriel Ziesemer Jr <adrieljr@adriel-jrs-macbook.local>
2015-09-21 16:56:21 -0300

    New Version

--- Astran/src/util.h
+++ Astran/src/util.h
@@ -23,7 +23,7 @@
 using namespace std;
 
 #define printme( e ) { cerr << #e << " = " << ( e ) << "\n"; };
-#define VERSION "3.9.7"
+#define VERSION "3.9.8"
 
 enum IOType {IOTYPE_INPUT, IOTYPE_OUTPUT, IOTYPE_INOUT};
 enum HorV {V, H};
