8377b43eb8dc3311504a6e57a75948648936dd9c
Leandro Heck <leoheck@gmail.com>
2015-10-13 15:49:09 -0300

    Improving support for command line

--- Astran/src/icpdApp.cpp
+++ Astran/src/icpdApp.cpp
@@ -11,34 +11,61 @@
 
 using namespace std;
 
-IMPLEMENT_APP(icpdFrmApp)
+IMPLEMENT_APP(icpdFrmApp);
 
-bool icpdFrmApp::OnInit(){
-	//frame->SetIcon(wxIcon(help_xpm));
+bool icpdFrmApp::OnInit()
+{
     setlocale(LC_ALL,"C");
-    if (argc == 2 && string(wxString(argv[1]).mb_str())=="--shell") {
+
+    ifstream ifile(cmdFilename.mb_str());
+    if ((!ifile) && (!cmdFilename.empty())) {
+        cout << "ERROR: File \'" << cmdFilename.mb_str() << "\' doesn't exist" << endl;
+        return false;
+    }
+
+    if (!gui_enabled) 
+    {
         DesignMng designmng;
         string cmd;
+
+        ifstream ifile(cmdFilename.mb_str());
+        if (ifile) 
+        {
+            cmd = string("read ") + string(wxString(cmdFilename).mb_str());
+            cout << "astran> " + cmd;
+            designmng.readCommand(cmd);
+        } else 
+        {
+            string astran_cfg;
+            astran_cfg = "astran.cfg";
+
+            wxString astran_path;
+            ::wxGetEnv(wxT("ASTRAN_PATH"), &astran_path);
+
+            if (wxDirExists(astran_path)) {
+                astran_cfg = string(wxString(astran_path).mb_str()) + "/bin/astran.cfg";
+            }
+
+            cmd = string("read ") + astran_cfg;
+            cout << "astran> " + cmd;
+            designmng.readCommand(cmd); 
+        }
+
         while (true) {
             cout << "astran> ";
             getline (cin, cmd);
             designmng.readCommand(cmd);
         }
-    }if (argc == 2 && string(wxString(argv[1]).mb_str())=="--help") {
-        cout << "Use: " << getFileName(string(wxString(argv[0]).mb_str())) << " [--shell] or [filename]" << endl;
-        exit(0);
-    }else if (argc == 2 ){
-		DesignMng designmng;
-		string cmd;
-        cmd= string("read ") + string(wxString(argv[1]).mb_str());
-		designmng.readCommand(cmd);
-		designmng.readCommand("exit");
-	} else{
+    } 
+    else
+    {
 		wxInitAllImageHandlers();
 		IcpdFrm *frame = new IcpdFrm(NULL);
 		SetTopWindow(frame);
+        // frame->SetIcon(wxIcon(help));
 		frame->Show();
 	}
+
     return true;
 }
 
--- Astran/src/icpdApp.h
+++ Astran/src/icpdApp.h
@@ -10,6 +10,11 @@
 #ifndef __ICPDFRMApp_h__
 #define __ICPDFRMApp_h__
 
+#include <iostream>
+#include <fstream>
+#include <wx/apptrait.h> 
+#include <wx/cmdline.h> 
+
 #ifndef WX_PRECOMP
 	#include <wx/wx.h>
 #else
@@ -18,11 +23,70 @@
 #include "icpdfrm.h"
 #include <wx/wx.h>
 
+
+class HybridTraits : public wxGUIAppTraits
+{ 
+	public: 
+		HybridTraits(bool gui_enabled) : wxGUIAppTraits(), 
+			gui_enabled(gui_enabled) { 
+		} 
+
+	private: 
+		bool gui_enabled; 
+}; 
+
+
+
 class icpdFrmApp : public wxApp
 {
 	public:
+		wxString cmdFilename; 
+
+	private:
+
 		bool OnInit();
 		int OnExit();
+
+		bool Initialize(int& argc, wchar_t **argv) { 
+ 
+			static const wxCmdLineEntryDesc desc[] = { 
+				{ wxCMD_LINE_SWITCH, wxT("s"), wxT("shell"), wxT("Run in shell mode") }, 
+				{ wxCMD_LINE_PARAM, NULL, NULL, wxT("FILENAME"), wxCMD_LINE_VAL_STRING, wxCMD_LINE_PARAM_OPTIONAL },
+				{ wxCMD_LINE_NONE }
+			}; 
+
+			// wxCmdLineParser parser;
+			wxCmdLineParser parser(desc, argc, argv); 
+			if (parser.Parse(true) != 0) { 
+				exit(1); 
+			} 
+
+			gui_enabled = !parser.Found(wxT("shell"));
+			cmdFilename = parser.GetParam(0);
+
+			if (gui_enabled) { 
+				// printf("Running in GUI\n");
+				return wxApp::Initialize(argc, argv); 
+			} else { 
+				// printf("Running in CLI mode\n");
+				return wxAppConsole::Initialize(argc, argv); 
+			}
+      } 
+
+	void CleanUp() { 
+		if (gui_enabled) { 
+			wxApp::CleanUp(); 
+		} else { 
+			wxAppConsole::CleanUp(); 
+		} 
+	} 
+
+	HybridTraits *CreateTraits() { 
+		return new HybridTraits(gui_enabled); 
+	} 
+
+	bool gui_enabled; 
+
 };
 
 #endif
